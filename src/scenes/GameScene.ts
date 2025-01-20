bd5ff4843377e42cf819c63f0178ff2cd118b7dd
warren-burke84 <71320524+warren-burke84@users.noreply.github.com>
2025-01-20 19:26:01 +0000

    Highlight a cell on the tile map
    Related to #16
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/warren-burke84/phaser-game/issues/16?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -4,6 +4,8 @@ export class GameScene extends Scene {
     private isDragging: boolean;
     private dragStartX: number;
     private dragStartY: number;
+    private graphics: Phaser.GameObjects.Graphics;
+    private coordinatesText: Phaser.GameObjects.Text;
 
     constructor() {
         super('GameScene');
@@ -45,12 +47,17 @@ export class GameScene extends Scene {
         }
 
         map.createLayer('Tile Layer 1', [ tileset1, tileset2 ]);
-        map.createLayer('Tile Layer 2', [ tileset1, tileset2 ]);
-        map.createLayer('Tile Layer 3', [ tileset1, tileset2 ]);
-        map.createLayer('Tile Layer 4', [ tileset1, tileset2 ]);
-        map.createLayer('Tile Layer 5', [ tileset1, tileset2 ]);
+        // map.createLayer('Tile Layer 2', [ tileset1, tileset2 ]);
+        // map.createLayer('Tile Layer 3', [ tileset1, tileset2 ]);
+        // map.createLayer('Tile Layer 4', [ tileset1, tileset2 ]);
+        // map.createLayer('Tile Layer 5', [ tileset1, tileset2 ]);
 
         this.placeHouses();
+
+        this.graphics = this.add.graphics();
+        this.coordinatesText = this.add.text(this.scale.width / 2, 20, '', {
+            fontFamily: 'Arial', fontSize: 16, color: '#ffffff'
+        }).setOrigin(0.5).setDepth(100);
 
         this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
             this.isDragging = true;
@@ -62,7 +69,11 @@ export class GameScene extends Scene {
             this.isDragging = false;
         });
 
-        this.input.on('pointermove', this.handleDrag, this);
+        this.input.on('pointermove', (pointer: Phaser.Input.Pointer) => {
+            this.handleDrag(pointer);
+            this.highlightCell(pointer);
+            this.displayCoordinates(pointer);
+        });
     }
 
     placeHouses() {
@@ -80,5 +91,36 @@ export class GameScene extends Scene {
             this.dragStartX = pointer.x;
             this.dragStartY = pointer.y;
         }
+    }
+
+    highlightCell(pointer: Phaser.Input.Pointer) {
+        const worldPoint = pointer.positionToCamera(this.cameras.main) as Phaser.Math.Vector2;
+        const tileX = Math.floor(worldPoint.x / 64);
+        const tileY = Math.floor(worldPoint.y / 64);
+
+        this.graphics.clear();
+        this.graphics.lineStyle(2, 0xffffff, 1);
+
+        const isoX = tileX * 64;
+        const isoY = tileY * 32;
+
+        this.graphics.beginPath();
+        this.graphics.moveTo(isoX, isoY);
+        this.graphics.lineTo(isoX + 32, isoY + 16);
+        this.graphics.lineTo(isoX, isoY + 32);
+        this.graphics.lineTo(isoX - 32, isoY + 16);
+        this.graphics.closePath();
+        this.graphics.strokePath();
+    }
+
+    displayCoordinates(pointer: Phaser.Input.Pointer) {
+        const worldPoint = pointer.positionToCamera(this.cameras.main) as Phaser.Math.Vector2;
+        const tileX = Math.floor(worldPoint.x / 64);
+        const tileY = Math.floor(worldPoint.y / 64);
+
+        const isoX = tileX * 64;
+        const isoY = tileY * 32;
+
+        this.coordinatesText.setText(`Tile: (${tileX}, ${tileY}) Iso: (${isoX}, ${isoY}) World: (${worldPoint.x}, ${worldPoint.y})`);
     }
 }
